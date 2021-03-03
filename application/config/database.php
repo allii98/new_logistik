b37e90330b07a8fd429e8a7be53b405bfac5b8e7
allii98 <alihore042@gmail.com>
2021-03-03 19:49:25 +0700

    perbaikan

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,36 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '192.168.1.231',
+	'hostname' => '192.168.1.237',
 	'username' => 'mis',
 	'password' => 'msaljkt@88',
-	'database' => 'db_stki',
+	'database' => 'new_logistikmsal_2021',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+$db['db_logistik_msal'] = array(
+	'dsn'	=> '',
+	// 'hostname' => 'localhost',
+	// 'username' => 'root',
+	// 'password' => '',
+	// 'hostname' => '192.168.1.231',
+	'hostname' => '192.168.1.237',
+	'username' => 'mis',
+	'password' => 'msaljkt@88',
+	'database' => 'new_logistikmsal_2021',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
