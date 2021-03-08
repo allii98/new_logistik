74ee353d5d313e80500533d81bdadf5dca8e3fcf
idrisabdul <idrisaziz52@gmail.com>
2021-03-08 13:10:15 +0700

    Update views v_login

--- application/modules/Login/views/v_login.php
+++ application/modules/Login/views/v_login.php
@@ -1,124 +1,129 @@
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title>Log In | UBold - Responsive Admin Dashboard Template</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
-        <meta content="Coderthemes" name="author" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="<?php echo base_url() ?>assets/images/favicon.ico">
 
-		<!-- App css -->
-		<link href="<?php echo base_url() ?>assets/css/bootstrap-creative.min.css" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
-		<link href="<?php echo base_url() ?>assets/css/app-creative.min.css" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
+<head>
+    <meta charset="utf-8" />
+    <title>Log In | UBold - Responsive Admin Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="<?php echo base_url() ?>assets/images/favicon.ico">
 
-		<link href="<?php echo base_url() ?>assets/css/bootstrap-creative-dark.min.css" rel="stylesheet" type="text/css" id="bs-dark-stylesheet" />
-		<link href="<?php echo base_url() ?>assets/css/app-creative-dark.min.css" rel="stylesheet" type="text/css" id="app-dark-stylesheet" />
+    <!-- App css -->
+    <link href="<?php echo base_url() ?>assets/css/bootstrap-creative.min.css" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
+    <link href="<?php echo base_url() ?>assets/css/app-creative.min.css" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
 
-		<!-- icons -->
-		<link href="<?php echo base_url() ?>assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url() ?>assets/css/bootstrap-creative-dark.min.css" rel="stylesheet" type="text/css" id="bs-dark-stylesheet" />
+    <link href="<?php echo base_url() ?>assets/css/app-creative-dark.min.css" rel="stylesheet" type="text/css" id="app-dark-stylesheet" />
 
-    </head>
+    <!-- icons -->
+    <link href="<?php echo base_url() ?>assets/css/icons.min.css" rel="stylesheet" type="text/css" />
 
-    <body class="loading authentication-bg authentication-bg-pattern">
+</head>
 
-        <div class="account-pages mt-5 mb-5">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-8 col-lg-6 col-xl-5">
-                        <div class="card bg-pattern">
+<body class="loading authentication-bg authentication-bg-pattern">
 
-                            <div class="card-body p-4">
-                                
-                                <div class="text-center w-75 m-auto">
-                                    <div class="auth-logo">
-                                        <a href="<?=base_url('Login')?>" class="logo logo-dark text-center">
-                                            <span class="logo-lg">
-                                                <img src="<?php echo base_url() ?>assets/images/logo-dark.png" alt="" height="22">
-                                            </span>
-                                        </a>
-                    
-                                        <a href="<?=base_url('Login')?>" class="logo logo-light text-center">
-                                            <span class="logo-lg">
-                                                <img src="<?php echo base_url() ?>assets/images/logo-light.png" alt="" height="22">
-                                            </span>
-                                        </a>
-                                    </div>
-                                    <p><?php echo $this->session->flashdata('pesan'); ?></p>
+    <div class="account-pages mt-5 mb-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-5">
+                    <div class="card bg-pattern">
+
+                        <div class="card-body p-4">
+
+                            <div class="text-center w-75 m-auto">
+                                <div class="auth-logo">
+                                    <a href="<?= base_url('Login') ?>" class="logo logo-dark text-center">
+                                        <span class="logo-lg">
+                                            <img src="<?php echo base_url() ?>assets/images/logo-dark.png" alt="" height="22">
+                                        </span>
+                                    </a>
+
+                                    <a href="<?= base_url('Login') ?>" class="logo logo-light text-center">
+                                        <span class="logo-lg">
+                                            <img src="<?php echo base_url() ?>assets/images/logo-light.png" alt="" height="22">
+                                        </span>
+                                    </a>
+                                </div>
+                                <p><?php echo $this->session->flashdata('pesan'); ?></p>
+                            </div>
+
+                            <form action="<?= base_url('Login/proses') ?>" method="POST">
+
+                                <div class="form-group mb-3">
+                                    <label for="emailaddress" style="font-family: Verdana, Geneva, Tahoma, sans-serif; font-size:small">Username</label>
+                                    <input class="form-control" type="text" id="username" name="username" required="true" placeholder="Enter your username" autocomplete="off">
                                 </div>
 
-                                <form action="<?=base_url('Login/proses')?>" method="POST">
-
-                                    <div class="form-group mb-3">
-                                        <label for="emailaddress">Username</label>
-                                        <input class="form-control" type="text" id="username" name="username" required="true" placeholder="Enter your username" autocomplete="off">
-                                    </div>
-
-                                    <div class="form-group mb-3">
-                                        <label for="password">Password</label>
-                                        <div class="input-group input-group-merge">
-                                            <input type="password" id="password" class="form-control" name="password" placeholder="Enter your password">
-                                            <div class="input-group-append" data-password="false">
-                                                <div class="input-group-text">
-                                                    <span class="password-eye"></span>
-                                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="password" style="font-family: Verdana, Geneva, Tahoma, sans-serif; font-size:small">Password</label>
+                                    <div class="input-group input-group-merge">
+                                        <input type="password" id="password" class="form-control" name="password" placeholder="Enter your password">
+                                        <div class="input-group-append" data-password="false">
+                                            <div class="input-group-text">
+                                                <span class="password-eye"></span>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="form-group mb-3">
-                                        <label for="emailaddress">Periode</label>
-                                        <input class="form-control" type="date" value="<?= date('Y-m-d') ?>"  id="periode" name="periode" required="" placeholder="Enter your Periode">
-                                    </div>
-                                    <div class="form-group mb-3">
-                                        <label for="emailaddress">PT</label>
-                                        <select class="form-control" id="pt_alias" name="pt_alias">
-                                            <option value="MSAL" selected="">PT MULIA SAWIT AGRO LESTARI</option>
-                                            <option value="MAPA">PT MITRA AGRO PERSADA ABADI</option>
-                                            <option value="PSAM">PT PERSADA SEJAHTERA AGRO MAKMUR</option>
-                                            <option value="PEAK">PT PERSADA ERA AGRO KENCANA</option>
-                                        </select>
-                                    </div>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="emailaddress" style="font-family: Verdana, Geneva, Tahoma, sans-serif; font-size:small">Periode</label>
+                                    <input class="form-control" type="date" value="<?= date('Y-m-d') ?>" id="periode" name="periode" style="font-family: Verdana, Geneva, Tahoma, sans-serif; font-size:small" required="" placeholder="Enter your Periode">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="emailaddress" style="font-family: Verdana, Geneva, Tahoma, sans-serif; font-size:small">PT</label>
+                                    <select class="form-control" id="pt_alias" name="pt_alias">
+                                        <option value="MSAL" selected="" style="font-family: Verdana, Geneva, Tahoma, sans-serif; font-size:small">PT MULIA SAWIT AGRO LESTARI</option>
+                                        <option value="MAPA" style="font-family: Verdana, Geneva, Tahoma, sans-serif; font-size:small">PT MITRA AGRO PERSADA ABADI</option>
+                                        <option value="PSAM" style="font-family: Verdana, Geneva, Tahoma, sans-serif; font-size:small">PT PERSADA SEJAHTERA AGRO MAKMUR</option>
+                                        <option value="PEAK" style="font-family: Verdana, Geneva, Tahoma, sans-serif; font-size:small">PT PERSADA ERA AGRO KENCANA</option>
+                                    </select>
+                                </div>
 
 
-                                    <div class="form-group mb-0 text-center">
-                                        <!-- <button class="btn btn-primary btn-block" type="submit"> Log In </button> -->
-                                        <input type="submit" class="btn btn-primary btn-block" name="submit" value="Log in">
-                                    </div>
 
-                                </form>
+                                <div class="form-group mb-2 text-center">
+                                    <!-- <button class="btn btn-primary btn-block" type="submit"> Log In </button> -->
+                                    <input type="submit" class="btn btn-primary btn-block" name="submit" style="font-family: Verdana, Geneva, Tahoma, sans-serif; font-size:small" value="Log in">
+                                </div>
 
-                               
+                                <div style="color: red; font-size:x-small; font-family: Verdana, Geneva, Tahoma, sans-serif;">
+                                    <i class="mdi mdi-information mr-1"></i>Tampilan terbaik dengan resolusi display 1366 x 768
+                                </div>
 
-                            </div> <!-- end card-body -->
-                        </div>
-                        <!-- end card -->
+                            </form>
+                        </div> <!-- end card-body -->
+                    </div>
+                    <!-- end card -->
 
-                        <div class="row mt-3">
-                            
-                        </div>
-                        <!-- end row -->
+                    <div class="row mt-3">
 
-                    </div> <!-- end col -->
-                </div>
-                <!-- end row -->
+                    </div>
+                    <!-- end row -->
+
+                </div> <!-- end col -->
             </div>
-            <!-- end container -->
+            <!-- end row -->
         </div>
-        <!-- end page -->
+        <!-- end container -->
+    </div>
+    <!-- end page -->
 
 
-        <footer class="footer footer-alt text-white-50">
-            2015 - <script>document.write(new Date().getFullYear())</script> &copy; UBold theme by <a href="" class="text-white-50">Coderthemes</a> 
-        </footer>
+    <footer class="footer footer-alt text-white-50">
+        <script>
+            document.write(new Date().getFullYear())
+        </script> &copy; <font face="verdana"> Copyright </font> <a href="http://msalgroup.com" style="color: #fff; font-family: Verdana, Geneva, Tahoma, sans-serif; font-size:small">PT Mulia Sawit Agro Lestari</a>
+    </footer>
 
-        <!-- Vendor js -->
-        <script src="<?php echo base_url() ?>assets/js/vendor.min.js"></script>
+    <!-- Vendor js -->
+    <script src="<?php echo base_url() ?>assets/js/vendor.min.js"></script>
 
-        <!-- App js -->
-        <script src="<?php echo base_url() ?>assets/js/app.min.js"></script>
-        
-    </body>
+    <!-- App js -->
+    <script src="<?php echo base_url() ?>assets/js/app.min.js"></script>
+
+</body>
+
 </html>
