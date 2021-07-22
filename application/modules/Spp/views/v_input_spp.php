0ac47169d627249508f728c93acc8a084dc8adb9
allii98 <alihore042@gmail.com>
2021-07-22 21:09:44 +0700

    perbaikan spp dilanjut si genza

--- application/modules/Spp/views/v_input_spp.php
+++ application/modules/Spp/views/v_input_spp.php
@@ -311,6 +311,16 @@
         });
 
         check_form_2();
+
+
+        $("#txt_qty_1").on("keypress keyup blur", function(event) {
+            //this.value = this.value.replace(/[^0-9\.]/g,'');
+            $(this).val($(this).val().replace(/[^0-9\.]/g, ''));
+            if ((event.which != 46 || $(this).val().indexOf('.') != -1) && (event.which < 48 || event.which > 57)) {
+                event.preventDefault();
+            }
+        });
+
     });
 
     function check_form_2() {
