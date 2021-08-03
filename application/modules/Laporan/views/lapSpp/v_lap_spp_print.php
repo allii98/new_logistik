849427fb521c2f77e55f924db02ea33a7ef99e37
allii98 <alihore042@gmail.com>
2021-08-03 18:25:34 +0700

    perbaikan laporan spp

--- application/modules/Laporan/views/lapSpp/v_lap_spp_print.php
+++ application/modules/Laporan/views/lapSpp/v_lap_spp_print.php
@@ -1,7 +1,9 @@
 <head>
     <style type="text/css">
         #tabel_spp tr td {
-            border: 1px solid #dddddd;
+            /*font-size: 12px;*/
+            border: 1px solid;
+            padding: 5px;
         }
 
         body {
@@ -19,8 +21,13 @@
         }
 
         #tr_content td.noborder {
+            /*border: none;*/
             border-bottom: 0px solid #FFF;
             border-top: 0px solid #FFF;
+        }
+
+        .warna_sebagian {
+            background-color: gray;
         }
     </style>
     <title>SPP - Surat Permintaan Pembelian</title>
@@ -62,7 +69,7 @@
         }
     }
     ?>
-    <h3 style="margin-bottom: 0px;"><?= $headTitle; ?> (<?= $lokasi ?>)</h3>
+    <h3><?= $ppo->devisi; ?> </h3>
     <?= $nama_kebun; ?>
     <table class="singleborder" border="1" width="10%" align="right">
         <tr>
@@ -89,7 +96,7 @@
                 Kode Departemen : <?= $ppo->kodedept . "-" . $ppo->namadept; ?><br />
             </td>
             <td colspan="4">Nomor Referensi &nbsp;: <?= $ppo->noreftxt; ?><br />
-                Tgl. Referensi &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : <?= $ppo->tglref; ?><br />
+                Tgl. Referensi &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : <?= date("d-m-Y", strtotime($ppo->tglref)); ?> <br />
                 Keterangan &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : <?= $ppo->ket; ?><br />
                 Status &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : <b><?= $ppo->status; ?></b><br />
             </td>
@@ -99,42 +106,111 @@
         </tr>
         <tr>
             <td align="center">NO.</td>
-            <td align="center">KODE</td>
-            <td align="center">NAMA BARANG</td>
+            <td align="center">KODE BARANG</td>
+            <td align="center" width="30%">NAMA BARANG</td>
             <td align="center">KUANTITAS</td>
-            <td align="center" width="10%">SISA STOK</td>
+            <td align="center">SISA STOK</td>
             <td align="center">SAT</td>
-            <td align="center">KETERANGAN</td>
+            <td align="center" width="30%">KETERANGAN</td>
         </tr>
 
         <?php
         $no = 1;
         foreach ($item_ppo as $list_item) {
+            if ($ppo->status2 == 2) {
+                if ($list_item->status2 == 0) {
         ?>
-            <tr id="tr_content">
-                <td class="noborder" align="center"><?= $no; ?></td>
-                <td class="noborder"><?= $list_item->kodebartxt; ?></td>
-                <td class="noborder"><?= $list_item->nabar; ?></td>
-                <td class="noborder" align="right"><?= $list_item->qty; ?></td>
-                <td class="noborder" align="right"><?= $list_item->STOK ?></td>
-                <td class="noborder" align="center"><?= $list_item->sat; ?></td>
-                <td class="noborder"><?= $list_item->ket; ?></td>
-            </tr>
+                    <tr id="tr_content">
+                        <td class="noborder" align="center"><?= $no; ?></td>
+                        <td class="noborder warna_sebagian"><?= $list_item->kodebartxt; ?></td>
+                        <td class="noborder"><?= $list_item->nabar; ?></td>
+                        <td class="noborder" align="center"><?= $list_item->qty; ?></td>
+                        <td class="noborder" align="center"><?= $list_item->STOK ?></td>
+                        <td class="noborder" align="center"><?= $list_item->sat; ?></td>
+                        <td class="noborder" style="word-break: break-word"><?= $list_item->ket; ?></td>
+                    </tr>
+                <?php
+                } else {
+                ?>
+                    <tr id="tr_content">
+                        <td class="noborder" align="center"><?= $no; ?></td>
+                        <td class="noborder"><?= $list_item->kodebartxt; ?></td>
+                        <td class="noborder"><?= $list_item->nabar; ?></td>
+                        <td class="noborder" align="center"><?= $list_item->qty; ?></td>
+                        <td class="noborder" align="center"><?= $list_item->STOK ?></td>
+                        <td class="noborder" align="center"><?= $list_item->sat; ?></td>
+                        <td class="noborder" style="word-break: break-word"><?= $list_item->ket; ?></td>
+                    </tr>
+                <?php
+                }
+            } elseif ($ppo->status2 == 0) {
+                ?>
+                <tr id="tr_content">
+                    <td class="noborder" align="center"><?= $no; ?></td>
+                    <td class="noborder"><?= $list_item->kodebartxt; ?></td>
+                    <td class="noborder"><?= $list_item->nabar; ?></td>
+                    <td class="noborder" align="center"><?= $list_item->qty; ?></td>
+                    <td class="noborder" align="center"><?= $list_item->STOK ?></td>
+                    <td class="noborder" align="center"><?= $list_item->sat; ?></td>
+                    <td class="noborder" style="word-break: break-word"><?= $list_item->ket; ?></td>
+                </tr>
+            <?php
+            } else {
+            ?>
+                <tr id="tr_content">
+                    <td class="noborder" align="center"><?= $no; ?></td>
+                    <td class="noborder"><?= $list_item->kodebartxt; ?></td>
+                    <td class="noborder"><?= $list_item->nabar; ?></td>
+                    <td class="noborder" align="center"><?= $list_item->qty; ?></td>
+                    <td class="noborder" align="center"><?= $list_item->STOK ?></td>
+                    <td class="noborder" align="center"><?= $list_item->sat; ?></td>
+                    <td class="noborder" style="word-break: break-word"><?= $list_item->ket; ?></td>
+                </tr>
         <?php
+            }
             $no++;
         }
         ?>
         <tr>
             <td colspan="2" align="center">Diminta,</td>
             <td colspan="1" align="center">Diperiksa,</td>
-            <td colspan="3" align="center">Disetujui,</td>
+            <td colspan="1" align="center">Diketahui,</td>
+            <td colspan="2" align="center">Disetujui,</td>
             <td colspan="1" align="center">Dibuat Oleh,</td>
         </tr>
         <tr>
-            <td colspan="2" align="center" height="50" valign="bottom">(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)</td>
-            <td colspan="1" align="center" height="50" valign="bottom">(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)</td>
-            <td colspan="3" align="center" height="50" valign="bottom">(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)</td>
-            <td colspan="1" align="center" height="50" valign="bottom">(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)</td>
+            <td colspan="2" align="center" height="70" valign="bottom">(<?= $ppo->user ?>)</td>
+            <td colspan="1" align="center" height="70" valign="bottom">
+                <?php if ($ppo->status2 == 1) {
+                ?>
+                    <img src="././assets/img/approved2.png" width="15%">
+                <?php
+                } else {
+                }
+                ?>
+                <br><?= 'KTU'; ?><br>
+            </td>
+            <td colspan="1" align="center" height="70" valign="bottom">
+                <?php if ($ppo->status2 == 1) {
+                ?>
+                    <img src="././assets/img/approved2.png" width="15%"><br>
+                <?php
+                } else {
+                }
+                ?>
+                <?= 'GM'; ?><br>
+            </td>
+            <td colspan="2" align="center" height="70" valign="bottom">
+                <?php if ($ppo->status2 == 1) {
+                ?>
+                    <img src="././assets/img/approved2.png" width="15%"><br>
+                <?php
+                } else {
+                }
+                ?>
+                <?= 'Dept Head'; ?><br>
+            </td>
+            <td colspan="1" align="center" height="70" valign="bottom">(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)</td>
         </tr>
     </table>
     <small><i>Tgl Cetak <?= date("d/m/Y H:i:s"); ?> - Client <?= $this->input->ip_address(); ?> <?= $this->platform->agent(); ?></i></small>
